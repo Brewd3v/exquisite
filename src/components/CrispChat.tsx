00f3ff6019525e35f78f29f76977d42d3ccd8aec
Brewd3v <james@brewdev.co.za>
2022-08-10 15:39:36 +0100

    Fix type errors for Crisp

--- src/components/CrispChat.tsx
+++ src/components/CrispChat.tsx
@@ -1,12 +1,19 @@
 import React, { useEffect } from "react";
 
+declare global {
+    interface Window {
+        $crisp: [];
+        CRISP_WEBSITE_ID: string;
+    }
+}
+
 function Crisp() {
     useEffect(() => {
         window.$crisp = [];
         window.CRISP_WEBSITE_ID = "39061251-c690-45cf-b9e8-6edb123ffadb";
         (() => {
-            const d = document;
-            const s = d.createElement("script");
+            const d: any = document;
+            const s: any = d.createElement("script");
             s.src = "https://client.crisp.chat/l.js";
             s.async = 1;
             d.getElementsByTagName("body")[0].appendChild(s);
